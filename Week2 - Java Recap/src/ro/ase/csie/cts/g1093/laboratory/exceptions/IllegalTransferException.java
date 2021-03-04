05a70d758cc0845f2eea8e531c3037df7af07530
gijumarina <gijuaurelia18@stud.ase.ro>
2021-03-04 14:30:13 +0200

    During lab

---
+++ Week2 - Java Recap/src/ro/ase/csie/cts/g1093/laboratory/exceptions/IllegalTransferException.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1093.laboratory.exceptions;
+
+public class IllegalTransferException extends Exception {
+
+	public IllegalTransferException(String message) {
+		super(message);
+	}
+	
+	public IllegalTransferException() {
+		
+	}
+}
