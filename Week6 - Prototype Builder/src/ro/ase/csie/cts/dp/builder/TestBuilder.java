3ec4eadeac4c96013be551aa99eae5cb024d0500
gijumarina <gijuaurelia18@stud.ase.ro>
2021-04-01 15:20:25 +0300

    Builder implementation

--- Week6 - Prototype Builder/src/ro/ase/csie/cts/dp/builder/TestBuilder.java
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/dp/builder/TestBuilder.java
@@ -5,14 +5,25 @@ public class TestBuilder {
 	public static void main(String[] args) {
 		
 		//1.create the object
-		SuperHero superHero = new SuperHero();
+//		SuperHero superHero = new SuperHero();
 		//2.init it
 		//TO DO: don't forget
 		
-		SuperHero superHero2 = 
-				new SuperHero("Superman",
-						100, false, false, 
-						new Weapon(), null, new Flying(), null);
+//		SuperHero superHero2 = 
+//				new SuperHero("Superman",
+//						100, false, false, 
+//						new Weapon(), null, new Flying(), null);
+//		
+		
+		SuperHero superman = 
+				new SuperHero.SuperHeroBuilder("Superman", 100).build();
+		SuperHero joker = 
+				new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain().
+				isWonded().
+				setRightWeapon(new Weapon()).
+				build();
+		
 		
 	}
 
