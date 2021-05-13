84ae2857b5321068a3114432eef738e9170d89d8
gijumarina <gijuaurelia18@stud.ase.ro>
2021-05-13 14:27:01 +0300

    Implementing Chain

---
+++ Week11 - Command and Chain/src/ro/ase/csie/cts/dp/chain/PrivateMessageModule.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.dp.chain;
+
+public class PrivateMessageModule extends GenericMessageHandler {
+
+	@Override
+	public void processMessage(Message message) {
+		
+		if(!message.destination.toLowerCase().equals("@everyone")) {
+			System.out.println("Private message for " + message.destination + " : " +
+					message.text);
+		}
+		else {
+			if(this.next != null) {
+				this.next.processMessage(message);
+			}
+		}
+		
+	}
+
+}
